08d7278c502afefb1f056b439570fac65dba5cf0
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-07-03 07:45:06 -0500

    repo create

---
+++ scraper.rb
@@ -0,0 +1,21 @@
+require 'selenium-webdriver'
+require 'nokogiri'
+
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')
+driver = Selenium::WebDriver.for(:chrome, options: options)
+
+# url = "https://tipti.market/Supermaxi/Frutas"
+url = 'https://www.supermercadosantamaria.com/categorias/frutas-y-verduras/02'
+driver.get(url)
+
+sleep 2
+
+html = driver.page_source
+
+page = Nokogiri::HTML(html)
+
+p page.css('p.nombre').text
+p page.css('p.precio').text
+
+driver.quit
