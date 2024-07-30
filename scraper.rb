53426f624eca2fe9e456de123c9e118de5df407c
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-07-30 14:46:18 -0500

    Testing scraper with offline html file

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,23 @@
-require 'selenium-webdriver'
+# require 'selenium-webdriver'
 require 'nokogiri'
 
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
-driver = Selenium::WebDriver.for(:chrome, options: options)
+# options = Selenium::WebDriver::Chrome::Options.new
+# options.add_argument('--headless')
+# driver = Selenium::WebDriver.for(:chrome, options: options)
 
 # url = "https://tipti.market/Supermaxi/Frutas"
-url = 'https://www.supermercadosantamaria.com/categorias/frutas-y-verduras/02'
-driver.get(url)
+# url = 'https://www.supermercadosantamaria.com/categorias/frutas-y-verduras/02'
+# driver.get(url)
 
-sleep 2
+# sleep 2
 
-html = driver.page_source
+# html = driver.page_source
+
+html = File.open('santamaria.html')
 
 page = Nokogiri::HTML(html)
 
 p page.css('p.nombre').text
 p page.css('p.precio').text
 
-driver.quit
+# driver.quit
