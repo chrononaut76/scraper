e0f43787dc45bfc5edfc29f0e63167a26a937c82
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-07-30 19:16:59 -0500

    Completed logic to scrape names and prices

--- scraper.rb
+++ scraper.rb
@@ -17,8 +17,26 @@ html = File.read('santamaria.html')
 
 page = Nokogiri::HTML(html)
 
-page.css('p.nombre a').each {|element| puts element}
+hash = {}
 
-page.css('p.precio span').each {|element| puts element}
+page.css("p.nombre, p.precio span").each do |element|
+  current_element = element.attributes.values[0]
+  if current_element.nil?
+    hash[hash.keys.last] = element.text.sub(/,/,".")[1..].to_f
+  else
+    case current_element.value
+    when "nombre"
+      hash[element.text.strip] = 0.0
+    when "tachado"
+      next
+    end
+  end
+end
+
+p hash
+
+# page.css('p.precio span').each do |price|
+#   p price.text if price.attributes.empty?
+# end
 
 # driver.quit
